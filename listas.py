fe7d2e7d54ea7cdec459de4dc041be122983d73c
AnaBotega2006 <aniahyt@gmail.com>
2024-11-30 11:01:19 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -2,5 +2,12 @@ from ObjetosPessoas import *
 
 clientes = [c1,c2,c3,c4]
 quartos = [q1,q2,q3,q4]
-reservas = [[0,2],[1,1],[2,0],[3,3]]
+quartos_alugados = []
+dias = [1,2,3,4]
+disponiveis_dia1 = [q1,q2,q3]
+disponiveis_dia2 = [q1,q2,q3]
+disponiveis_dia3 = [q1]
+disponiveis_dia4 = [q1]
+reservas = [["34",0,2],["12",1,1]]
 
+#quarto id 2 pessoa id 0 reservado do dia 03 ao 04
