ca3c51370748f026df3be88d60108eb1aad29dc3
caroladilino <cmqadilino@gmail.com>
2024-11-30 19:51:17 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -1,11 +1,13 @@
 from Classes import *
 from ObjetosPessoas import *
+from listas import*
+from Funcoes import*
 
 #listas de controle
 
 
 #variaveis
-#id_cliente = len(clientes)-1
+id_cliente = len(clientes)-1
 #codigo = ""
 
 choice = 1
@@ -15,6 +17,7 @@ while choice != 0:
     print("=" * 3 , "OPCOES" ,"=" * 20 , )
     print("Já é nosso cliente ? \n[1] Sim \n[2] Nao , quero me casdastrar \n[0] Sair ")
     choice = int(input("Resposta : "))
+    choice = check(0,2,choice) 
     print("="*31)
 
     if choice == 1 : #Se ja for cliente
@@ -28,12 +31,19 @@ while choice != 0:
             nome_novo = input("Nome : ")
             idade_novo = int(input("Idade : "))
             sexo_novo = int(input("Sexo [0]Homem [1]Mulher : "))
+            sexo_novo = check(0,1,sexo_novo)
             vip_novo = int(input("Gostaria de ser um cliente VIP? [0]Nao [1]Sim "))
+            vip_novo = check(0,1,vip_novo)
             if vip_novo == 0 :
                 print("Gostaria de algum adicionar ? \n[1]Open bar \n[2]Acesso a todos os restaurantes \n[3]Acesso aos shows")
                 codigo1 = int(input("Acesso ao Open bar      [0]Nao [1]Sim"))
+                codigo1 = check(0,1,codigo1)
                 codigo2 = int(input("Acesso aos restaurantes [0]Nao [1]Sim"))
+                codigo2 = check(0,1,codigo2)
                 codigo3 = int(input("Acesso aos shows        [0]Nao [1]Sim"))
+                codigo3 = check(0,1,codigo3)
+            
+
                 acessos_novo = f"{codigo1}" + f"{codigo2}" + f"{codigo3}"
                 a = Cliente(nome_novo, idade_novo, sexo_novo, vip_novo, acessos_novo, id_cliente)
                 print(f"O cliente de {nome_novo} foi adicionado a lista com id {id_cliente}")
