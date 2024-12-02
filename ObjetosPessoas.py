8a16b3d9d4c528647ece1f15d7802f4b3bf4a0a1
caroladilino <cmqadilino@gmail.com>
2024-12-02 13:40:40 -0300

    Update ObjetosPessoas.py

--- ObjetosPessoas.py
+++ ObjetosPessoas.py
@@ -1,30 +1,33 @@
 from Classes import *
 
-c1 = Cliente("Paulo",40 , 0 , 0 , "000",0)
-Cliente("Neusa",34 , 1 , 0 , "110",1)
-VIP("Joana",40 , 1 , 1 , "111",2)
 
 
 
-c1 = Cliente("Paulo",40 , 0 , 0 , "000",0)
+
+#(self,quantd1 , camas1, andar1, varanda1,id1):
 q1 = Quarto(4, "2C0S", 1, 1,0)
+q2 = Quarto(2, "2S0S", 2, 0,1)
+q3 = Quarto(3, "1C1S", 1, 0,2)
+q4 = QuartoVIP(4 , "1C2S", 3, 1,3,1,3)
+q5 = QuartoVIP(2 , "1C0S", 3, 1,3,1,4)
+q6 = Quarto(3, "1C1S", 1, 1,5)
+q7 = Quarto(4, "2C0S", 2, 1,6)
+q8 = Quarto(2, "0C2S", 1, 0,7)
 
-c2 = Cliente("Neusa",34 , 1 , 0 , "110",1)
-q2 = Quarto(1, "1S0S", 2, 0,1)
 
 
-c3 = Cliente("Leandro",18 , 0 , 0 , "000",2)
-q3 = Quarto(2, "1C0S", 3, 0,2)
+# nome1, idade1, sexo1,vip1,acessos1,id1):
+c1 = Cliente("Paulo",40 , 0 , 0 , "111",0)
+c2 = Cliente("Neusa",34 , 1 , 0 , "000",1)
+c3 = Cliente("Leandro",18 , 0 , 0 , "010",2)
+c4 = VIP    ("Luciana",30 , 1 , 1 , "111",3)
+c5 = Cliente("Mauro",80 , 0 , 0 , "000",4)
+c6 = Cliente("Thais",30 , 1 , 0 , "000",5)
+c7 = VIP    ("Flavia",35 , 1 , 1 , "111",6)
+c8 = VIP    ("Christina",40 , 1 , 1 , "111",7)
 
+#adicionar elemento de consumo
+#reservas = [["17",0,0] , ["25",1,1] , ["89",2,2] , ["17",3,3] , ["17",4,4],["12",5,5] , ["34",6,6] , ["89",7,2] , ["17",3,3] , ["17",4,4]]
 
-c4 = VIP("Joana",40 , 1 , 1 , "111",3)
-q4 = QuartoVIP(4 , "1C2S", 4, 1,3,1,0)
-
-#Paulo id 0 tem o quarto 3 id 2
-#Neusa id 1 tem o quarto 2 id 1
-#Leandro id 2 tem o quarto 1 id 0
-#Joana id 3 tem o quarto 4 id 3
-
-#reservas = [[0,2][1,1],[2,0],[3,3]]
 
 
